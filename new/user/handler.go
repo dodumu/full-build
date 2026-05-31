119a761718d131af1a79424b7bc2907b447f9951
dodumu <davidrobertq2@gmail.com>
2026-05-31 20:35:34 +0100

    settings

--- new/user/handler.go
+++ new/user/handler.go
@@ -11,10 +11,37 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Hanlder1(w http.ResponseWriter, r *http.Request) {
+
 	users, err := LoadUsers("user/some.json")
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	json.NewEncoder(w).Encode(users)
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case http.MethodGet:
+		err = json.NewEncoder(w).Encode(users)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+		var newuser User
+
+		err = json.NewDecoder(r.Body).Decode(&newuser)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = AddUser(users, newuser)
+		err = SaveUsers("new.json", users)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 }
